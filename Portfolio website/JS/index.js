a5fac1b7a5b83cf107f616e1966235edae70d54d
MD SAIF <98917571+12saif@users.noreply.github.com>
2022-12-07 08:44:48 +0530

    Add files via upload

---
+++ Portfolio website/JS/index.js
@@ -0,0 +1,8 @@
+// /* ================== typing animation =============*/
+var typed = new Typed(".typing", {
+    Strings: ["", "Web Developer", "Web Designer", "Graphic Designer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+// /* ================= Aside =====================*/
